a5b8a5ca69253019b47ff5cef6e255894ad4555d
Saulo Soares <35870646+saulo308@users.noreply.github.com>
2026-04-10 05:59:37 -0300

    (Feat): Added AbilitySet
    AbilitySet holds info on GAs + GEs + AttributeSets to give/take from an ASC

---
+++ Source/TDWTechnicalTest/AbilitySystem/AbilitySet.cpp
@@ -0,0 +1,157 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "AbilitySet.h"
+#include "TDWTechnicalTest/TWDTechnicalTestLogging.h"
+#include "TDWTechnicalTest/AbilitySystem/TDWTestGameplayAbility.h"
+#include "TDWTechnicalTest/AbilitySystem/TDWTestAbilitySystemComponent.h"
+
+void FAbilitySet_GrantedHandles::AddAbilitySpecHandle(
+	const FGameplayAbilitySpecHandle& Handle)
+{
+	if (Handle.IsValid())
+	{
+		AbilitySpecHandles.Add(Handle);
+	}
+}
+
+void FAbilitySet_GrantedHandles::AddGameplayEffectHandle(
+	const FActiveGameplayEffectHandle& Handle)
+{
+	if (Handle.IsValid())
+	{
+		GameplayEffectHandles.Add(Handle);
+	}
+}
+
+void FAbilitySet_GrantedHandles::AddAttributeSet(UAttributeSet* Set)
+{
+	GrantedAttributeSets.Add(Set);
+}
+
+void FAbilitySet_GrantedHandles::TakeFromAbilitySystem(
+	UAbilitySystemComponent* TargetASC)
+{
+	check(TargetASC);
+	if (!TargetASC->IsOwnerActorAuthoritative())
+	{
+		TDWTestLog_ERROR(TEXT("AbilitySets must only be removed from "
+			"server. Failed to remove ability set."));
+		return;
+	}
+
+	for (const FGameplayAbilitySpecHandle& Handle : AbilitySpecHandles)
+	{
+		if (Handle.IsValid())
+		{
+			TargetASC->ClearAbility(Handle);
+		}
+	}
+
+	for (const FActiveGameplayEffectHandle& Handle : GameplayEffectHandles)
+	{
+		if (Handle.IsValid())
+		{
+			TargetASC->RemoveActiveGameplayEffect(Handle);
+		}
+	}
+
+	for (UAttributeSet* Set : GrantedAttributeSets)
+	{
+		TargetASC->RemoveSpawnedAttribute(Set);
+	}
+
+	AbilitySpecHandles.Reset();
+	GameplayEffectHandles.Reset();
+	GrantedAttributeSets.Reset();
+}
+
+void UAbilitySet::GiveToAbilitySystem(
+	UAbilitySystemComponent* TargetASC,
+	FAbilitySet_GrantedHandles* OutGrantedHandles, UObject* SourceObject) const
+{
+	check(TargetASC);
+	if (!TargetASC->IsOwnerActorAuthoritative())
+	{
+		TDWTestLog_ERROR(TEXT("AbilitySets must only be granted from "
+			"server. Failed to give [%s] ability set."), *GetName());
+		return;
+	}
+
+	// Grant the gameplay abilities from the ability set
+	for (int32 AbilityIndex = 0; AbilityIndex < GrantedGameplayAbilities.Num();
+		++AbilityIndex)
+	{
+		// Get the ability to grant
+		const auto& AbilityToGrant = GrantedGameplayAbilities[AbilityIndex];
+		if (!IsValid(AbilityToGrant.Ability))
+		{
+			TDWTestLog_ERROR(TEXT("GrantedGameplayAbilities [%d] on ability "
+				"set [%s] is not valid."), AbilityIndex, *GetNameSafe(this));
+			continue;
+		}
+
+		auto* AbilityCDO = AbilityToGrant.Ability->GetDefaultObject<
+			UTDWTestGameplayAbility>();
+
+		// Create the ability spec, passing the source objet and tags
+		FGameplayAbilitySpec AbilitySpec(AbilityCDO,
+			AbilityToGrant.AbilityLevel);
+		AbilitySpec.SourceObject = SourceObject;
+		AbilitySpec.GetDynamicSpecSourceTags().AddTag(AbilityToGrant.InputTag);
+
+		// Give the ability to ASC
+		const auto AbilitySpecHandle = TargetASC->GiveAbility(AbilitySpec);
+		
+		if (OutGrantedHandles)
+		{
+			OutGrantedHandles->AddAbilitySpecHandle(AbilitySpecHandle);
+		}
+	}
+
+	// Grant the gameplay effects from ability set.
+	for (int32 EffectIndex = 0; EffectIndex < GrantedGameplayEffects.Num();
+		++EffectIndex)
+	{
+		const auto& EffectToGrant =	GrantedGameplayEffects[EffectIndex];
+		if (!IsValid(EffectToGrant.GameplayEffect))
+		{
+			TDWTestLog_ERROR(TEXT("GrantedGameplayEffects[%d] on ability set "
+				"[%s] is not valid"), EffectIndex, *GetNameSafe(this));
+			continue;
+		}
+
+		// Apply the GE
+		const auto* GameplayEffect = EffectToGrant.GameplayEffect->
+			GetDefaultObject<UGameplayEffect>();
+		const FActiveGameplayEffectHandle GameplayEffectHandle =
+			TargetASC->ApplyGameplayEffectToSelf(GameplayEffect,
+			EffectToGrant.EffectLevel, TargetASC->MakeEffectContext());
+
+		if (OutGrantedHandles)
+		{
+			OutGrantedHandles->AddGameplayEffectHandle(GameplayEffectHandle);
+		}
+	}
+
+	// Grant the attribute sets from the ability set.
+	for (int32 SetIndex = 0; SetIndex < GrantedAttributes.Num(); ++SetIndex)
+	{
+		const auto& SetToGrant = GrantedAttributes[SetIndex];
+		if (!IsValid(SetToGrant.AttributeSet))
+		{
+			TDWTestLog_ERROR(TEXT("GrantedAttributes[%d] on ability set "
+				"[%s] is not valid"), SetIndex, *GetNameSafe(this));
+			continue;
+		}
+
+		// Add the attribute set
+		auto* NewSet = NewObject<UAttributeSet>(TargetASC->GetOwner(),
+			SetToGrant.AttributeSet);
+		TargetASC->AddAttributeSetSubobject(NewSet);
+
+		if (OutGrantedHandles)
+		{
+			OutGrantedHandles->AddAttributeSet(NewSet);
+		}
+	}
+}
---
+++ Source/TDWTechnicalTest/AbilitySystem/AbilitySet.h
@@ -0,0 +1,121 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "ActiveGameplayEffectHandle.h"
+#include "Engine/DataAsset.h"
+#include "AttributeSet.h"
+#include "GameplayTagContainer.h"
+#include "GameplayAbilitySpecHandle.h"
+#include "AbilitySet.generated.h"
+
+/**	Data used by the ability set to grant gameplay abilities. */
+USTRUCT(BlueprintType)
+struct FAbilitySet_GameplayAbility
+{
+	GENERATED_BODY()
+
+	/** Gameplay ability to grant. */
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<class UTDWTestGameplayAbility> Ability;
+
+	/** Level of ability to grant. */
+	UPROPERTY(EditDefaultsOnly)
+	int32 AbilityLevel = 1;
+
+	/** Tag used to process input for the ability. */
+	UPROPERTY(EditDefaultsOnly, Meta = (Categories = "InputTag"))
+	FGameplayTag InputTag = FGameplayTag();
+};
+
+/** Data used by the ability set to grant gameplay effects. */
+USTRUCT(BlueprintType)
+struct FAbilitySet_GameplayEffect
+{
+	GENERATED_BODY()
+
+	/** Gameplay effect to grant. */
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<class UGameplayEffect> GameplayEffect = nullptr;
+
+	/** Level of gameplay effect to grant. */
+	UPROPERTY(EditDefaultsOnly)
+	float EffectLevel = 1.0f;
+};
+
+/** Data used by the ability set to grant attribute sets. */
+USTRUCT(BlueprintType)
+struct FAbilitySet_AttributeSet
+{
+	GENERATED_BODY()
+
+	/** Gameplay effect to grant. */
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<UAttributeSet> AttributeSet;
+};
+
+/** Data used to store handles to what has been granted by the ability set. */
+USTRUCT(BlueprintType)
+struct FAbilitySet_GrantedHandles
+{
+	GENERATED_BODY()
+
+	/** */
+	void TakeFromAbilitySystem(class UAbilitySystemComponent* TargetASC);
+	
+	/** */
+	void AddAbilitySpecHandle(const FGameplayAbilitySpecHandle& Handle);
+	
+	/** */
+	void AddGameplayEffectHandle(const FActiveGameplayEffectHandle& Handle);
+	
+	/** */
+	void AddAttributeSet(UAttributeSet* Set);
+
+protected:
+	/** Handles to the granted abilities. */
+	UPROPERTY()
+	TArray<FGameplayAbilitySpecHandle> AbilitySpecHandles;
+
+	/** Handles to the granted gameplay effects. */
+	UPROPERTY()
+	TArray<FActiveGameplayEffectHandle> GameplayEffectHandles;
+
+	/** Pointers to the granted attribute sets. */
+	UPROPERTY()
+	TArray<TObjectPtr<UAttributeSet>> GrantedAttributeSets;
+};
+
+
+/**
+* Non-mutable data asset used to grant gameplay abilities and gameplay effects.
+*/
+UCLASS(BlueprintType, Const)
+class UAbilitySet : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	/**
+	* Grants the ability set to the specified ability system component.
+	* 
+	* The returned handles can be used later to take away anything that was
+	* granted.
+	*/ 
+	void GiveToAbilitySystem(class UAbilitySystemComponent* TargetASC,
+		FAbilitySet_GrantedHandles* OutGrantedHandles,
+		UObject* SourceObject = nullptr) const;
+
+protected:
+	/** Gameplay abilities to grant when this ability set is granted. */
+	UPROPERTY(EditDefaultsOnly, Category = "Gameplay Abilities")
+	TArray<FAbilitySet_GameplayAbility> GrantedGameplayAbilities;
+
+	/** Gameplay effects to grant when this ability set is granted. */
+	UPROPERTY(EditDefaultsOnly, Category = "Gameplay Effects")
+	TArray<FAbilitySet_GameplayEffect> GrantedGameplayEffects;
+
+	/** Attribute sets to grant when this ability set is granted. */
+	UPROPERTY(EditDefaultsOnly, Category = "Attribute Sets")
+	TArray<FAbilitySet_AttributeSet> GrantedAttributes;
+};
