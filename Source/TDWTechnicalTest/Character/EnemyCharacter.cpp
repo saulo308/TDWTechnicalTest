4ffc6c25966dfac7c4d20ae9f4eef977700faae3
Saulo Soares <35870646+saulo308@users.noreply.github.com>
2026-04-11 14:22:07 -0300

    (Feat): Added ASC to dummy enemy

---
+++ Source/TDWTechnicalTest/Character/EnemyCharacter.cpp
@@ -0,0 +1,74 @@
+// Copyright Saulo Soares. All rights reserved.
+
+
+#include "EnemyCharacter.h"
+#include "TDWTechnicalTest/TWDTechnicalTestLogging.h"
+#include "TDWTechnicalTest/AbilitySystem/AbilitySet.h"
+#include "TDWTechnicalTest/AbilitySystem/TDWTestAbilitySystemComponent.h"
+#include "TDWTechnicalTest/Character/Data/PawnData.h"
+
+AEnemyCharacter::AEnemyCharacter()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	
+	AbilitySystemComponent = CreateDefaultSubobject<
+		UTDWTestAbilitySystemComponent>(TEXT("AbilitySystem"));
+}
+
+void AEnemyCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AEnemyCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void AEnemyCharacter::PossessedBy(AController* NewController)
+{
+	Super::PossessedBy(NewController);
+	
+	InitAbilitySystemComponent();
+
+	if (const auto LoadedPawnData = DefaultPawnData.LoadSynchronous())
+	{
+		SetPawnData(LoadedPawnData);
+	}
+}
+
+void AEnemyCharacter::InitAbilitySystemComponent()
+{
+	// Init the ability actor info
+	check(AbilitySystemComponent);
+	AbilitySystemComponent->InitAbilityActorInfo(this, this);
+	
+	TDWTestLog_INFO(TEXT("Initialized character [%s] ASC."), *GetName());
+}
+
+void AEnemyCharacter::SetPawnData(const TObjectPtr<UPawnData>& PawnData)
+{
+	if (!PawnData)
+	{
+		TDWTestLog_WARNING(TEXT("No pawn data was passed to enemy character."));
+		return;
+	}
+
+	CurrentPawnData = PawnData;
+
+	// Foreach ability set from pawn data, give it to the player's ASC
+	for (const auto AbilitySet : PawnData->AbilitySets)
+	{
+		if (!AbilitySet)
+		{
+			continue;
+		}
+		
+		AbilitySet->GiveToAbilitySystem(AbilitySystemComponent, nullptr);
+	}
+}
+
+UAbilitySystemComponent* AEnemyCharacter::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComponent;
+}
---
+++ Source/TDWTechnicalTest/Character/EnemyCharacter.h
@@ -0,0 +1,67 @@
+// Copyright Saulo Soares. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemInterface.h"
+#include "GameFramework/Character.h"
+#include "EnemyCharacter.generated.h"
+
+/**
+* 
+*/
+UCLASS()
+class TDWTECHNICALTEST_API AEnemyCharacter : public ACharacter,
+	public IAbilitySystemInterface
+{
+	GENERATED_BODY()
+
+public:
+	/** Sets default values for this character's properties */
+	AEnemyCharacter();
+
+public:
+	/** */
+	UFUNCTION(BlueprintCallable)
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+	
+public:
+	/** Called every frame */
+	virtual void Tick(float DeltaTime) override;
+	
+	/**
+	* Called once the Character is possessed on the server. Will initialize
+	* the ASC on the server.
+	*/
+	virtual void PossessedBy(AController* NewController) override;
+	
+protected:
+	/** Called when the game starts or when spawned */
+	virtual void BeginPlay() override;
+
+private:
+	/**
+	* Initializes the ASC. Will call "InitAbilityActorInfo" on it, passing the
+	* PlayerState as owner and the Character as the avatar.
+	*/
+	void InitAbilitySystemComponent();
+	
+	/** */
+	void SetPawnData(const TObjectPtr<class UPawnData>& PawnData);
+	
+protected:
+	/** */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly,
+		Category="TDWTestCharacter|PawnData")
+	TSoftObjectPtr<class UPawnData> DefaultPawnData;
+	
+private:
+	/** */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite,
+		meta=(AllowPrivateAccess="true"))
+	TObjectPtr<class UTDWTestAbilitySystemComponent> AbilitySystemComponent;
+	
+	/** */
+	UPROPERTY()
+	TObjectPtr<class UPawnData> CurrentPawnData;
+};
