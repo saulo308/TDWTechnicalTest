f7f5b8dd184ab6c0f6b612941f373c1edcab11a9
Saulo Soares <35870646+saulo308@users.noreply.github.com>
2026-04-10 22:08:05 -0300

    (Feat): First prototype of leap ability task

--- Source/TDWTechnicalTest/AbilitySystem/AbilityTasks/AbilityTask_LeapToTargetAndWait.cpp
+++ Source/TDWTechnicalTest/AbilitySystem/AbilityTasks/AbilityTask_LeapToTargetAndWait.cpp
@@ -3,3 +3,99 @@
 
 #include "AbilityTask_LeapToTargetAndWait.h"
 
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "TDWTechnicalTest/TWDTechnicalTestLogging.h"
+
+
+UAbilityTask_LeapToTargetAndWait::UAbilityTask_LeapToTargetAndWait()
+{
+	bTickingTask = true;
+}
+
+UAbilityTask_LeapToTargetAndWait* UAbilityTask_LeapToTargetAndWait::Leap(
+	UGameplayAbility* OwningAbility, FVector InTargetLocation,
+	UCurveFloat* InHorizontalCurve, UCurveFloat* InHeightCurve)
+{
+	UAbilityTask_LeapToTargetAndWait* Task =
+		NewAbilityTask<UAbilityTask_LeapToTargetAndWait>(OwningAbility);
+
+	Task->TargetLocation = InTargetLocation;
+	Task->HorizontalCurve = InHorizontalCurve;
+	Task->HeightCurve = InHeightCurve;
+	
+	return Task;
+}
+
+void UAbilityTask_LeapToTargetAndWait::TickTask(float DeltaTime)
+{
+	Super::TickTask(DeltaTime);
+
+	if (bIsLeaping)
+	{
+		UpdateLeap(DeltaTime);
+	}
+}
+
+void UAbilityTask_LeapToTargetAndWait::Activate()
+{
+	auto* Character = Cast<ACharacter>(GetAvatarActor());
+	if (!Character)
+	{
+		EndTask();
+		return;
+	}
+
+	StartLeap();
+}
+
+void UAbilityTask_LeapToTargetAndWait::StartLeap()
+{
+	auto* Character = Cast<ACharacter>(GetAvatarActor());
+
+	LeapStartLocation = Character->GetActorLocation();
+	TargetLocation.Z +=
+		Character->GetCapsuleComponent()->GetUnscaledCapsuleHalfHeight();
+	ElapsedTime = 0.f;
+
+	Character->GetCharacterMovement()->DisableMovement();
+	bIsLeaping = true;
+}
+
+void UAbilityTask_LeapToTargetAndWait::UpdateLeap(float DeltaTime)
+{
+	ElapsedTime += DeltaTime;
+	CurrentLeapProgress = FMath::Clamp(ElapsedTime / LeapDuration, 0.f, 1.f);
+
+	const auto HorizontalAlpha = HorizontalCurve->GetFloatValue(
+		CurrentLeapProgress);
+	const auto HorizontalPosition = UKismetMathLibrary::VLerp(LeapStartLocation,
+		TargetLocation, HorizontalAlpha);
+
+	const auto ArcHeight = HeightCurve->GetFloatValue(CurrentLeapProgress) *
+		MaxArcHeight;
+	const auto CurrentHeight = FVector(0.f, 0.f, ArcHeight);
+
+	const auto FinalPos = HorizontalPosition + CurrentHeight;
+	
+	auto* Character = Cast<ACharacter>(GetAvatarActor());
+	Character->SetActorLocation(FinalPos);
+
+	if (CurrentLeapProgress >= 1.f)
+	{
+		FinishLeap();
+	}
+}
+
+void UAbilityTask_LeapToTargetAndWait::FinishLeap()
+{
+	bIsLeaping = false;
+
+	auto* Character = Cast<ACharacter>(GetAvatarActor());
+	Character->GetCharacterMovement()->SetMovementMode(MOVE_Walking);
+	
+	OnLanded.Broadcast();
+	EndTask();
+}
--- Source/TDWTechnicalTest/AbilitySystem/AbilityTasks/AbilityTask_LeapToTargetAndWait.h
+++ Source/TDWTechnicalTest/AbilitySystem/AbilityTasks/AbilityTask_LeapToTargetAndWait.h
@@ -6,6 +6,8 @@
 #include "Abilities/Tasks/AbilityTask.h"
 #include "AbilityTask_LeapToTargetAndWait.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FLeapFinished);
+
 /**
  * 
  */
@@ -15,4 +17,65 @@ class TDWTECHNICALTEST_API UAbilityTask_LeapToTargetAndWait :
 {
 	GENERATED_BODY()
 	
+public:
+	/** */
+	UAbilityTask_LeapToTargetAndWait();
+	
+public:
+	/** */
+	UFUNCTION(BlueprintCallable, Category="Ability|Tasks",
+		meta = (DisplayName="LeapToTargetAndWait",
+		HidePin = "OwningAbility", DefaultToSelf = "OwningAbility",
+		BlueprintInternalUseOnly = "TRUE"))
+	static UAbilityTask_LeapToTargetAndWait* Leap(
+		UGameplayAbility* OwningAbility, FVector InTargetLocation,
+		class UCurveFloat* InHorizontalCurve, class UCurveFloat* InHeightCurve);
+
+public:
+	/** Tick function for this task, if bTickingTask == true */
+	virtual void TickTask(float DeltaTime) override;
+	
+	/** */
+	virtual void Activate() override;
+	
+protected:
+	/** */
+	virtual void StartLeap();
+
+	/** */
+	virtual void UpdateLeap(float DeltaTime);
+
+	/** */
+	virtual void FinishLeap();
+	
+public:
+	/** */
+	UPROPERTY(BlueprintAssignable)
+	FLeapFinished OnLanded;
+	
+protected:
+	UPROPERTY()
+	TObjectPtr<class UCurveFloat> HorizontalCurve;
+	
+	UPROPERTY()
+	TObjectPtr<class UCurveFloat> HeightCurve;
+
+protected:
+	/** */
+	FVector TargetLocation = FVector();
+	
+	/** */
+	float MaxArcHeight = 400.f;
+	
+private:
+	/** */
+	FVector LeapStartLocation = FVector();
+
+	float ElapsedTime = 0.f;
+
+	bool bIsLeaping = false;
+
+	float CurrentLeapProgress = 0.f;
+
+	float LeapDuration = 1.f;
 };
