b36be4b22a726ccc6f44f5e317bef4f2b04e2f98
Saulo Soares <35870646+saulo308@users.noreply.github.com>
2026-04-11 09:39:24 -0300

    (Feat): Added targeting BP library
    Helper for targeting using GAS

---
+++ Source/TDWTechnicalTest/AbilitySystem/BlueprintLibraries/GASTargetingLibrary.cpp
@@ -0,0 +1,5 @@
+// Copyright Saulo Soares. All rights reserved.
+
+
+#include "GASTargetingLibrary.h"
+
---
+++ Source/TDWTechnicalTest/AbilitySystem/BlueprintLibraries/GASTargetingLibrary.h
@@ -0,0 +1,18 @@
+// Copyright Saulo Soares. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "GASTargetingLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TDWTECHNICALTEST_API UGASTargetingLibrary :
+	public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
