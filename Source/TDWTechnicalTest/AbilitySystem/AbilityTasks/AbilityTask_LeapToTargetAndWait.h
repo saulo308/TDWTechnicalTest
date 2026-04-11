cb02e3562786442b94a0567e542312ff67363d43
Saulo Soares <35870646+saulo308@users.noreply.github.com>
2026-04-10 21:41:15 -0300

    (Feat): Added leap to target AbilityTask class

---
+++ Source/TDWTechnicalTest/AbilitySystem/AbilityTasks/AbilityTask_LeapToTargetAndWait.h
@@ -0,0 +1,18 @@
+// Copyright Saulo Soares. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/Tasks/AbilityTask.h"
+#include "AbilityTask_LeapToTargetAndWait.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TDWTECHNICALTEST_API UAbilityTask_LeapToTargetAndWait :
+	public UAbilityTask
+{
+	GENERATED_BODY()
+	
+};
