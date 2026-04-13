0a8a72ce7267bed9c5f8c8357ef53d9c005b7e99
Saulo Soares <35870646+saulo308@users.noreply.github.com>
2026-04-13 19:04:36 -0300

    (Feat): Added stun tags

--- Source/TDWTechnicalTest/TDWTestTags.cpp
+++ Source/TDWTechnicalTest/TDWTestTags.cpp
@@ -7,6 +7,8 @@ namespace TDWTestGameplayTags
 	UE_DEFINE_GAMEPLAY_TAG_COMMENT(InputTag_Move, "InputTag.Move", "Move input.");
 	
 	UE_DEFINE_GAMEPLAY_TAG_COMMENT(AbilityCooldownTag, "Data.Cooldown", "Cooldown GE tag to set cooldown duration with \"SetByCaller\".");
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(AbilityStunTag, "Data.Stun", "Stun GE tag to set stun duration with \"SetByCaller\".");
 
 	UE_DEFINE_GAMEPLAY_TAG_COMMENT(LeapMovementState, "State.Movement.Leaping", "Movement state indicating the character is leaping;");
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(StunMovementState, "State.Movement.Stun", "Movement state indicating the character is stunned;");
 }
